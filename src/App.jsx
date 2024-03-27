89c99e1f9f1f1b650a89e2d3c942b0ec987075af
Mozaffar <FERAWWWESS@gmail.com>
2024-03-27 15:07:09 +0300

    calc-design

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,40 @@
 import React from "react";
-import "./App.css";
+import "./App.scss";
+import { squaresArray } from "./squaresArray";
+import { IoBackspaceOutline } from "react-icons/io5";
+import { TbMath } from "react-icons/tb";
+import { CgMathPlus } from "react-icons/cg";
+import { CgMathPercent } from "react-icons/cg";
+import { CgMathMinus } from "react-icons/cg";
 
 function App() {
+  const [operation, setOperation] = React.useState(0);
+  
+  /* Show symbol based on item text */
+  const showSymbol = (item) => {
+    if (item === "erase") {
+      return <IoBackspaceOutline size={23} />;
+    } else {
+      return item;
+    }
+  };
+
   return (
-    <>
-      <h1>Calc</h1>
-    </>
+    <div className="app flexCenterColumn">
+      <div className="result flexEnd">{operation}</div>
+      <div className="squares flexCenter">
+        {squaresArray.map((item, index) => {
+          return (
+            <div
+              className={`square flexCenter ${item == "=" && "equal-bg"}`}
+              key={index}
+            >
+              {showSymbol(item)}
+            </div>
+          );
+        })}
+      </div>
+    </div>
   );
 }
 
